2b7fa9882407e1238bd3a0d598143966a6b77dff
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2024-10-31 15:05:15 +0100

    CI fix attempt

--- cargo_publish.sh
+++ cargo_publish.sh
@@ -7,12 +7,7 @@ cargo update -vw
 
 
 (cd src/proc_macros
-    cargo publish
+    cargo +stable publish
 )
 
-for i in $(seq 10)
-do
-    cargo publish && exit 0
-    sleep 5
-done
-cargo publish
+cargo +stable publish
