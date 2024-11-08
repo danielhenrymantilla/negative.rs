3294177c227069ab80b35ce992a3dc4086a36340
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2024-11-08 13:50:59 +0100

    Replace AI picture with human-made one
      - Credit to @FlawlessDeveloper

--- src/_lib.rs
+++ src/_lib.rs
@@ -1,7 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![no_std]
 #![forbid(unsafe_code)]
-#![doc(html_logo_url = "https://gist.github.com/user-attachments/assets/8dc46e20-90d6-431a-8805-665549e02650")]
+#![doc(html_logo_url = "https://gist.github.com/user-attachments/assets/d0dc0b98-aa31-4078-8164-ae6f04d4b6e7")]
 
 /// See the [`crate`] top-level docs for more info.
 pub use ::negative_proc_macros::negative_impl;
